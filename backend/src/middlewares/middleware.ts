a02ba312fbe826c0196ff8c3b2e1744b9f76e396
aviralshukla-06 <aviralshukla733@gmail.com>
2025-08-02 00:04:20 +0530

    added a middleware

---
+++ backend/src/middlewares/middleware.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express"
+import { AuthRequest } from "./types"
+import dotenv from "dotenv"
+import jwt from "jsonwebtoken"
+dotenv.config()
+const secret = process.env.JWT_SECRET
+
+export const userMiddleware = (req: AuthRequest, res
+    : Response, next: NextFunction) => {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !secret) {
+        throw new Error("Either secret or token is not defined");
+        return;
+    }
+
+    try {
+        const decodedToken = jwt.verify(authHeader, secret) as { id: number }
+
+        if (decodedToken) {
+            req.userId = decodedToken.id;
+            next();
+        }
+    } catch (err) {
+        res.status(403).json({
+            message: "Invalid or Expired Token"
+        });
+        return
+    }
+
+
+}
