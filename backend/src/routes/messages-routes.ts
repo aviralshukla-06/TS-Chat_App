fada6f8748b421a38f8c37731c8bca40f30f095e
aviralshukla-06 <aviralshukla733@gmail.com>
2025-08-07 01:31:32 +0530

    completed backend

--- backend/src/routes/messages-routes.ts
+++ backend/src/routes/messages-routes.ts
@@ -25,4 +25,44 @@ app.get("/users", userMiddleware, async (req: AuthRequest, res: Response) => {
     // }
 })
 
+app.get("/chatMsgData/:id", userMiddleware, async (req: AuthRequest, res: Response) => {
+    const userToChatId = req.params.id;
+    const loggedInUserId = req.userId;
+    if (!loggedInUserId) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        });
+    }
+
+    const messages = ` SELECT * FROM "Messages" WHERE ("senderId" = $1 AND "receiverId" = $2) OR ("senderId" = $2 AND "receiverId" = $1) ORDER BY "timeStamp" ASC;`
+    const messageResponse = await pgClient.query(messages, [userToChatId, loggedInUserId])
+
+    const exchangedUserMessages = messageResponse.rows;
+
+    res.status(200).json({
+        exchangedUserMessages
+    })
+})
+
+
+app.post("/messages/:id", userMiddleware, async (req: AuthRequest, res: Response) => {
+    const receiverId = req.params.id;
+    const senderId = req.userId;
+
+    try {
+        const { text, image } = req.body;
+
+        const newMessage = `INSERT INTO "Messages" ("senderId", "receiverId", "text", "image") VALUES ($1, $2, $3, $4);`
+        const newMsgData = await pgClient.query(newMessage, [senderId, receiverId, text, image]);
+
+        res.status(200).json({
+            newMsgData,
+            message: "message sent successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+})
 export default app;
