37969828ebc0caf6c1b0ff5caed4685d0a707e15
aviralshukla-06 <aviralshukla733@gmail.com>
2025-08-10 23:46:35 +0530

    created components

--- chat-frontend/src/App.tsx
+++ chat-frontend/src/App.tsx
@@ -1,26 +1,55 @@
+import { Routes, Route } from "react-router-dom"
+// import NavBar from "./components/NavBar"
+// import { HomePage } from "./pages/HomePage"
+// import SignupPage from "./pages/SignupPage"
+// import SigninPage from "./pages/SigninPage"
+// import SettingPage from "./pages/SettingPage"
+// import ProfilePage from "./pages/ProfilePage"
+import { CardHeader } from "./components/CardHeader"
 import { UserIcon } from "./custom-icons/UserIcon"
 
-function App() {
-  {/* <h1 className="flex bg-"></h1> */ }
 
+const App = () => {
   return (
-
-    <div className="flex justify-center items-center h-screen">
-      <div className="h-96 w-96 border border-black flex items-center font-sans bg-blue-500 custom-diagonal-bg">
-        <div className="flex items-center justify-center bg-inherit">
-          <div className="border border-black rounded-full flex justify-center items-center z-10  h-12 w-12 bg-white">
-            <UserIcon size="lg" />
-          </div>
-          <div className="-ml-4 z-0">
-            <input className="bg-[#476468] h-9 flex justify-center rounded-r-3xl" placeholder="username" />
-          </div>
-
-        </div>
-        {/* hi there */}
-      </div>
+    <div>
+      {/* <NavBar/> */}
+      {/* <BrowserRouter> */}
+      <Routes>
+        <Route path="/" element={<CardHeader
+          profileIcon={<UserIcon size="lg" />}
+          headingText={"Create Account"}
+          generalText={"Welcome"}
+        />} />
+        {/* <Route path="/signup" element={<SignupPage />} />
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/settings" element={<SettingPage />} />
+          <Route path="/profile" element={<ProfilePage />} /> */}
+      </Routes>
+      {/* </BrowserRouter> */}
     </div>
-
   )
+
 }
 
+
+
 export default App
+
+
+//  return (
+
+//     <div className="flex justify-center items-center h-screen">
+//       <div className="h-96 w-96 border border-black flex items-center font-sans bg-blue-500 custom-diagonal-bg">
+//         <div className="flex items-center justify-center bg-inherit">
+//           <div className="border border-black rounded-full flex justify-center items-center z-10  h-12 w-12 bg-white">
+//             <UserIcon size="lg" />
+//           </div>
+//           <div className="-ml-4 z-0">
+//             <input className="bg-[#476468] h-9 flex justify-center rounded-r-3xl" placeholder="username" />
+//           </div>
+
+//         </div>
+//       </div>
+//     </div>
+
+//   )
--- chat-frontend/src/custom-icons/IconSize.tsx
+++ chat-frontend/src/custom-icons/IconSize.tsx
@@ -7,6 +7,6 @@ export interface SizeStyles {
 
 export const sizeClasses = {
     sm: "w-5 h-5 ", // You can customize width/height or padding
-    md: "w-6 h-6 ",
-    lg: "w-6 h-8 ",
+    md: "w-6 h-8  ",
+    lg: "w-10 h-12",
 }
